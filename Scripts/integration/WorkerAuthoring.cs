770577e311b400b03eec43b5a53e6de243503c08
I-Protecc <ryanbaumeister@googlemail.com>
2024-07-02 17:48:22 +0200

    feat: unit movement (WIP)

--- Scripts/integration/WorkerAuthoring.cs
+++ Scripts/integration/WorkerAuthoring.cs
@@ -1,8 +1,31 @@
+using GameJamPlaceHolderName.Scripts.domain;
 using Godot;
 
 namespace GameJamPlaceHolderName.Scripts.integration;
 
-public class WorkerAuthoring : Node2D
+public partial class WorkerAuthoring : Node2D
 {
-    
+    [Export]
+    public int Health;
+
+    public Worker Worker { get; private set; }
+	
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Worker = new Worker(Health);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (Worker.IsDead) QueueFree();
+        
+        
+    }
+
+    public void Damage(float damage)
+    {
+        Worker.Damage(damage);
+    }
 }
