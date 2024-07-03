0eb8af6f249169ea8c9cc715aa06a454afb18ec7
SzyZu <skwarek116@gmail.com>
2024-07-03 21:41:52 +0200

    chore: add domain building

---
+++ Scripts/domain/Building.cs
@@ -0,0 +1,25 @@
+﻿namespace GameJamPlaceHolderName.Scripts.domain;
+
+public class Building
+{
+    public float Health { get; private set; }
+    private HealthPool _healthPool;
+
+    public bool IsDestroyed => Health <= 0; 
+
+    public Building(float health)
+    {
+        this.Health = health; 
+        _healthPool = new HealthPool(Health, 0, health);
+    }
+    
+    public void Damage(float damage)
+    {
+        Health = _healthPool.DecreaseHealth(damage);
+    }
+    
+    public void Kill()
+    {
+        Damage(Health);
+    }
+}
