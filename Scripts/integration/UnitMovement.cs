b547f181535da4c7f4588b6834eaf5901d0552de
SzyZu <skwarek116@gmail.com>
2024-07-03 18:18:57 +0200

    bugs: i dont even know what i did anymore; nothing works

--- Scripts/integration/UnitMovement.cs
+++ Scripts/integration/UnitMovement.cs
@@ -6,16 +6,17 @@ namespace GameJamPlaceHolderName.Prefabs;
 
 public partial class UnitMovement : CharacterBody2D
 {
-	private NavigationAgent2D _navigationAgent;
-
 	private float _movementSpeed = 200.0f;
-
+	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	
 	private WorkerAuthoring _worker;
+	
+	private NavigationAgent2D _navigationAgent;
 
 	public Vector2 MovementTarget
 	{
 		get { return _navigationAgent.TargetPosition; }
-		set { _navigationAgent.TargetPosition = new Vector2(value.X, GlobalPosition.Y); }
+		set { _navigationAgent.TargetPosition = value; }
 	}
 
 	public override void _Ready()
@@ -38,35 +39,34 @@ public partial class UnitMovement : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		base._PhysicsProcess(delta);
+		Vector2 velocity = Velocity;
+		Vector2 direction = Vector2.Zero;
+		velocity.Y += Gravity * (float)delta;
 
-		if (_navigationAgent.IsNavigationFinished())
+		if (!_navigationAgent.IsNavigationFinished())
 		{
-			return;
+			direction = _navigationAgent.GetNextPathPosition() - GlobalPosition;
+			direction = direction.Normalized();
 		}
 
-		Vector2 currentAgentPosition = GlobalTransform.Origin;
-		Vector2 nextPathPosition = _navigationAgent.GetNextPathPosition();
-
-		Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * _movementSpeed;
-		MoveAndSlide();
+		velocity.X = direction.X * _movementSpeed;
+		Velocity = velocity;
+		MoveAndCollide(Velocity * (float)delta);
 	}
 
 	private async void ActorSetup()
 	{
 		// Wait for the first physics frame so the NavigationServer can sync.
 		await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
-		//MovementTarget = _movementTargetPosition;
 	}
 
 	private void _onMouseEntered()
 	{
-		GD.Print("Hovering over worker I see");
 		_worker.MouseInside = true;
 	}
 	
 	private void _onMouseExited()
 	{
-		GD.Print("Stopped hovering over worker");
 		_worker.MouseInside = false;
 	}
 }
