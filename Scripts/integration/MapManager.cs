a5e95e22cb2a19c7e6d0d2e1c3155b1e81840856
I-Protecc <ryanbaumeister@googlemail.com>
2024-07-06 02:36:43 +0200

    refactor: make Initialize method private (the caffeine is hitting)

--- Scripts/integration/MapManager.cs
+++ Scripts/integration/MapManager.cs
@@ -8,7 +8,7 @@ public partial class MapManager : TileMap
 {
     Dictionary<Vector2, int> _tileHealth = new Dictionary<Vector2, int>();
 
-    public void InitializeTileMap(TileMap tileMap)
+    private void InitializeTileMap(TileMap tileMap)
     {
         foreach (Vector2I pos in tileMap.GetUsedCells(0))
         {
